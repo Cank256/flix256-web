5d912db5fae4610a76a5df99bfa1da19d2c05433
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-01-18 20:40:02 +0300

    Handle method not allowed error

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, jsonify
 from flask_pymongo import PyMongo
 from flask_redis import FlaskRedis
 import os
@@ -20,6 +20,19 @@ redis_client = FlaskRedis(app)
 # TMDB URL and API Key to be used for fetching data
 TMDB_URL = os.getenv('TMDB_URL')
 TMDB_API_KEY = os.getenv('TMDB_API_KEY')
+
+@app.errorhandler(405)
+def method_not_allowed(e):
+    """
+    Custom error handler for HTTP 405 Method Not Allowed errors.
+
+    Args:
+        e: The error object.
+
+    Returns:
+        A JSON response with an error message and the 405 status code.
+    """
+    return jsonify(error="This method is not allowed for the requested URL."), 405
 
 # Import views
 from backend import movies
