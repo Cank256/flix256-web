be2f66df576e444f70843633db4c33532c261534
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-01-28 10:40:18 +0300

    Setup the frontend

---
+++ frontend/components/Home.vue
@@ -0,0 +1,26 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+  >
+    <link
+      href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
+      rel="stylesheet"
+    />
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <a
+        class="flex justify-center pt-8 sm:pt-0"
+        href="#"
+        target="_blank"
+      >
+        <img alt="Flix256" src="../static/splash.png" width="200"/>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+}
+</script>
