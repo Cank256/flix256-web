a0c96d9489e6f365bc35e13de7f8ce6f2f7ecb8f
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-01-30 12:28:34 +0300

    Frontend: Add the Footer

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -3,17 +3,20 @@
         <NavBar v-if="!$device.isMobileOrTablet" />
         <MobileNav v-else />
         <slot />
+        <Footer />
     </div>
 </template>
 
 <script>
     import NavBar from '~/components/global/NavBar';
     import MobileNav from '~/components/global/MobileNav';
+    import Footer from '~/components/global/Footer';
     
     export default {
         components: {
             NavBar,
             MobileNav,
+            Footer,
         },
     };
 </script>
