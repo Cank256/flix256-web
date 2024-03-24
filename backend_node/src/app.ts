d223857c72c43f8dafa4cb946036ca2bd35e8c75
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-03-24 10:15:15 +0300

    backend: Add MongoDB configs

--- backend_node/src/app.ts
+++ backend_node/src/app.ts
@@ -6,7 +6,7 @@ const http = require('http').Server(app)
 const morgan = require('morgan')
 
 const router = require('./config/router')
-const redis = require('./config/redis')
+const connectDB = require('./config/db')
 
 app.use(helmet())
 app.use(cors())
@@ -14,6 +14,7 @@ app.use(bodyParser.json())
 app.use(morgan('dev'))
 
 app.use(router)
+connectDB()
 
 http.listen(3000, () => {
     console.log('Server is running on http://localhost:3000')
