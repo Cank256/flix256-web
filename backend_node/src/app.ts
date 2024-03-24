eb2eefa8195f46bc363ad60785f74207e4a4e52a
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-03-24 10:45:29 +0300

    Backend: Handle 404 error

--- backend_node/src/app.ts
+++ backend_node/src/app.ts
@@ -1,9 +1,10 @@
+import { error } from "console"
+
 const app = require('express')()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const helmet = require('helmet')
 const http = require('http').Server(app)
-const morgan = require('morgan')
 
 const router = require('./config/router')
 const connectDB = require('./config/db')
@@ -11,10 +12,26 @@ const connectDB = require('./config/db')
 app.use(helmet())
 app.use(cors())
 app.use(bodyParser.json())
-app.use(morgan('dev'))
 
 app.use(router)
 connectDB()
+
+app.use((req, res, next) => {
+    const err: any = new Error('Not Found')
+    err.status = 404
+    next(err)
+})
+
+app.use((err, req, res, next) => {
+    if (err.status === 404) {
+        let response = {
+            status: err.status || 404,
+            message: err.message || 'Route Not Found.'
+        
+        }
+        res.status(err.status).json(response)
+    }
+})
 
 http.listen(3000, () => {
     console.log('Server is running on http://localhost:3000')
